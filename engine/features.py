76c1be64149442584a7b844047e61af121725009
pinky-Kumari-Saha <pinkykumarisaha362@gmail.com>
2024-12-21 19:20:41 +0530

    day3 complete

---
+++ engine/features.py
@@ -0,0 +1,6 @@
+from playsound import playsound
+
+#playing assistant sound function
+def playAssistantSound():
+    music_dir="www\\assets\\audio\\energetic-pop-sound-ringtone-245162.mp3"
+    playsound(music_dir)
